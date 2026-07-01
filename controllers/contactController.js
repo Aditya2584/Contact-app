f0ad5ec747f7ea5f197e9f291fb94d23e57cf1d5
Aditya2584 <bestrong2584@gmail.com>
2026-07-01 14:02:25 +0530

    Error Handling

---
+++ controllers/contactController.js
@@ -0,0 +1,48 @@
+// @desc Get All conatct
+// @router GET /api/contact
+// @access public
+const getContacts = (req, res)=>{
+    res.status(200).json({message: "Get All conatact"})
+};
+
+// @desc Create All conatct
+// @router POST /api/contact
+// @access public
+const CreateContact = (req, res)=>{
+    console.log("The request body is :", req.body);
+    const {name, email, phone} = req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    res.status(201).json({message: "Create conatact"})
+};
+
+// @desc Get conatact
+// @router GET /api/contact/:id
+// @access public
+const getContact = (req, res)=>{
+    res.status(200).json({message : `Get contacts for ${req.params.id}`});
+}
+
+// @desc update conatact
+// @router PUT /api/contact/:id
+// @access public
+const UpdateContact = (req, res)=>{
+    res.status(200).json({message : `Update contacts for ${req.params.id}`});
+}
+
+// @desc update conatact
+// @router PUT /api/contact/:id
+// @access public
+const DeleteContact = (req, res)=>{
+    res.status(200).json({message : `Delete contacts for ${req.params.id}`});
+}
+
+module.exports = {
+    getContacts,
+    CreateContact,
+    getContact,
+    UpdateContact,
+    DeleteContact,
+}
